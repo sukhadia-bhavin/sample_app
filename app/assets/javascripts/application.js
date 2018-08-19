dd383431fc7ddf5fd1664de65e870fe0e75d6c69
Bhavin Sukhadia <sukhadia.bhavin@gmail.com>
2018-08-19 15:06:50 +0530

    Final Commit Of Ch-8

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery-3.3.1
+//= require bootstrap
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
